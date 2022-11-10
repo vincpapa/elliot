00670bf5f048e95e6e80bc2818c246d4c6caa0dd
vincpapa <vincenzo.paparella@poliba.it>
2022-11-10 13:27:24 +0100

    my modifications

--- elliot/recommender/graph_based/__init__.py
+++ elliot/recommender/graph_based/__init__.py
@@ -1,3 +1,17 @@
-from .ngcf import NGCF
-from .lightgcn import LightGCN
-from .RP3beta import RP3beta
+def import_model_by_backend(tensorflow_cmd, pytorch_cmd):
+    import sys
+    for _backend in sys.modules["external"].backend:
+        if _backend == "tensorflow":
+            exec(tensorflow_cmd)
+        elif _backend == "pytorch":
+            exec(pytorch_cmd)
+            break
+
+
+import sys
+for _backend in sys.modules["external"].backend:
+    if _backend == "tensorflow":
+        pass
+    elif _backend == "pytorch":
+        from .ngcf import NGCF
+        from .lightgcn import LightGCN
